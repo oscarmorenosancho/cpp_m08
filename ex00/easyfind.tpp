e1c3d010dc1448b52c317641270150c3ef74bd39
Oscar Moreno Sancho <omoreno-@car9s2.42barcelona.com>
2023-10-07 18:41:40 +0200

    ex00 seams to work

--- ex00/easyfind.tpp
+++ ex00/easyfind.tpp
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 10:58:19 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/10/07 15:32:18 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/10/07 18:36:29 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 # define _EASYFIND_TPP_
 # include <vector>
 # include <algorithm>
+# include <exception>
 
 template <typename Container>
 int easyfind(const Container& ctnr, int n)
 {
-	int it = std::find(ctnr.begin(), ctnr.end(), n)[0];
-	return (it);
+	const int it = std::find(ctnr.begin(), ctnr.end(), n)[0];
+  	if (it != *ctnr.end())
+		return (it);
+  	else
+    	throw std::runtime_error("Exception: Element Not Found in Container");	
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,44 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 10:53:53 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/10/06 16:44:45 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/10/07 18:39:42 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <easyfind.hpp>
 #include <iostream>
 
-int main(int, char const **)
+int main(int argc, char const **argv)
 {
+	int to_locate;
+
+	if (argc != 2)
+	{
+		std::cerr << "Error:" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " number_to_locate"<< std::endl;
+		return (1);
+	}
+	try
+	{
+		to_locate = std::stoi(argv[1]);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return (1);
+	}
+	
 	std::vector<int> test;
 	for (int i = 0; i < 10; i++)
-		test[i] = i;
-	int number = easyfind(test, 5);
-	std::cout << number << std::endl;
+		test.push_back(i * 10);
+	try
+	{
+		int number = easyfind(test, to_locate);
+		std::cout << number << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
